57a808317e6b97474f456bcedf89ee9a2e660ccc
Emerson Siega <emersonsiega@hotmail.com>
2018-10-27 19:07:27 -0300

    feat: Add Loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,14 +3,19 @@ import { receiveUsers } from "../actions/users"
 import { receiveTweets } from "../actions/tweets"
 import { setAuthedUser } from "../actions/authedUser"
 
+import { showLoading, hideLoading } from "react-redux-loading"
+
 const AUTHED_ID = 'emersonsiega'
 
-const handleInitialData = () => dispatch => 
+const handleInitialData = () => dispatch => {
+    dispatch(showLoading())
     getInitialData().then(({users, tweets}) => {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
         dispatch(setAuthedUser(AUTHED_ID))
+        dispatch(hideLoading())
     })
+}
 
 export {
     AUTHED_ID,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,13 @@ import users from "../reducers/users"
 import tweets from "../reducers/tweets"
 import authedUser from "../reducers/authedUser"
 
+import { loadingBarReducer } from "react-redux-loading"
+
 const rootReducer = combineReducers({
     authedUser,
     users,
     tweets,
+    loadingBar: loadingBarReducer,
 })
 
 export default rootReducer
